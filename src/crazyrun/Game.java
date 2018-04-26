ad1113ff8eed8bc8d6bbcc4060566924bb239712
hosseini sajad <hosseni_sajjad@yahoo.com>
2018-04-26 16:13:41 +0430

    bug

--- src/crazyrun/Game.java
+++ src/crazyrun/Game.java
@@ -7,22 +7,19 @@ import java.awt.event.ActionListener;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
-public class Game extends JPanel implements ActionListener {
+public class Game extends JPanel {
 
 	public static final int WIDTH = 300;
 	public static final int HEIGHT = 600;
 	public static final int SCORE_HEIGHT = 120;
 	
 	// game objects
-	private Ball ball;
-	private Board board;
-	private Timer timer;
+	private Player player;
+
 	
 	public Game() {
-		ball = new Ball(WIDTH / 2, 0, 40, Color.BLUE);
-		board = new Board(WIDTH / 2 - 100, HEIGHT - SCORE_HEIGHT - 70, Color.BLACK, 200, 20);
-		timer = new Timer(30, this);
-		timer.start();
+		player = new Player(WIDTH / 2, (HEIGHT - (player.getSize())), 40, Color.BLUE);
+		
 	}
 	
 	@Override
@@ -31,33 +28,15 @@ public class Game extends JPanel implements ActionListener {
 
 		// ----------game design---------------------
 		setBackground(Color.WHITE);
-		ball.draw(g);
-		board.draw(g);
+		player.draw(g);
+		
 		
 		//collision detection
-		if(ball.getBound().intersects(board.getBound()))
-			ball.moveUp();
+		
 		
 		// ----------score design----------------------
-		g.setColor(Color.DARK_GRAY);
-		g.fillRect(0, HEIGHT - SCORE_HEIGHT, WIDTH, SCORE_HEIGHT);
-		
-		g.setColor(Color.YELLOW);
-		g.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 14));
-		
-		g.drawString("Your Name: Ali", 10, HEIGHT - SCORE_HEIGHT + 30);
-		g.drawString("Your Score: 0", 10, HEIGHT - SCORE_HEIGHT + 50);
-		
-		g.setColor(Color.CYAN);
-		g.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 12));
-		g.drawString("Powered By: M.Yasrebi", WIDTH - 150, HEIGHT - 20);	
 		
 		
 	}
 
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		ball.move(WIDTH,HEIGHT - SCORE_HEIGHT);
-		repaint();
-	}
 }
--- src/crazyrun/Player.java
+++ src/crazyrun/Player.java
@@ -1,5 +1,55 @@
 package crazyrun;
 
+import java.awt.Color;
+import java.awt.Graphics;
+
 public class Player {
+    private int x;
+    private int y;
+    private int size;
+    private Color color;
+
+    public Player(int x, int y, int size, Color color) {
+        this.x = x;
+        this.y = y;
+        this.size = size;
+        this.color = color;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
     
+    public void draw(Graphics g) {
+        g.setColor(getColor());
+        g.fillRect(getX(), getY(), getSize(), getSize());
+    }
 }
