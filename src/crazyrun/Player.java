f24171089c53bcf2d322277bcc2246f926c05a04
hosseini sajad <hosseni_sajjad@yahoo.com>
2018-04-26 16:16:15 +0430

    bug

--- src/crazyrun/Player.java
+++ src/crazyrun/Player.java
@@ -10,10 +10,10 @@ public class Player {
     private Color color;
 
     public Player(int x, int y, int size, Color color) {
-        this.x = x;
-        this.y = y;
-        this.size = size;
-        this.color = color;
+        setX(x);
+        setY(y);
+        setSize(size);
+        setColor(color);
     }
 
     public int getX() {
