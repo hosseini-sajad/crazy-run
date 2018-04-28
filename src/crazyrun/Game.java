fc3ab4e80117ad626d2a0aad2f9821678c612235
hosseini sajad <hosseni_sajjad@yahoo.com>
2018-04-28 22:24:28 +0430

    change complete

--- src/crazyrun/Game.java
+++ src/crazyrun/Game.java
@@ -7,27 +7,19 @@ import java.awt.event.ActionListener;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
-<<<<<<< HEAD
-public class Game extends JPanel implements ActionListener{
-
-	public static final int WIDTH = 600;
-	public static final int HEIGHT = 800;
-        public static final int ROAD_HEIGHT = 220;
-        
-=======
 public class Game extends JPanel implements ActionListener {
 
 	public static final int WIDTH = 600;
 	public static final int HEIGHT = 800;
+        public static final int ROAD_HEIGHT = 220;
         public static final int RIVER_HEIGHT = 220; 
->>>>>>> Amir
 	
         // game font
         Font gameFont = new Font("Press Start 2P", Font.BOLD, 26);
         
 	// game objects
 	private Player player;
-<<<<<<< HEAD
+        private River river;
         public Road road;
         private Timer timer;
         
@@ -35,19 +27,11 @@ public class Game extends JPanel implements ActionListener {
 	public Game() {
 		player = new Player(280, 760, 40, Color.WHITE);
                 road = new Road(0, HEIGHT - 350, WIDTH, ROAD_HEIGHT, Color.decode("#434343"));
+                river = new River(0, 70 ,WIDTH,RIVER_HEIGHT, Color.decode("#008dc7"));
                 timer = new Timer(30, this);
 		timer.start();
-=======
-        private River river;
-        private Timer timer;
 
 	
-	public Game() {
-		player = new Player( 280, 760, 40, Color.WHITE);
-		river = new River(0, 70 ,WIDTH,RIVER_HEIGHT, Color.decode("#008dc7"));
-                timer = new Timer(30, this);
-                timer.start();
->>>>>>> Amir
 	}
 	
 	@Override
@@ -57,14 +41,9 @@ public class Game extends JPanel implements ActionListener {
 		// ----------game design---------------------
 		setBackground(Color.decode("#cda360"));
 		player.draw(g);
-<<<<<<< HEAD
                 road.draw(g);
-=======
                 river.draw(g);
                
-                /*draw game river*/
->>>>>>> Amir
-                
 		
 		//collision detection
 		
@@ -81,17 +60,9 @@ public class Game extends JPanel implements ActionListener {
 	}
 
     @Override
-<<<<<<< HEAD
     public void actionPerformed(ActionEvent e) {
         road.move();
-        repaint();
-    }
-    
-=======
-    public void actionPerformed(ActionEvent ae) {
         river.Move();
         repaint();
     }
-     
->>>>>>> Amir
 }
