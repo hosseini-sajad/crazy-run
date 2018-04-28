b3bb7e49dc02ab42ba60247fa95f7297dd580ad4
AmirHR7 <amirrassaei1376@gmail.com>
2018-04-28 20:19:21 +0430

    my tasks i is ok now
    my tasks is ok now

--- src/crazyrun/Game.java
+++ src/crazyrun/Game.java
@@ -7,19 +7,26 @@ import java.awt.event.ActionListener;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
-public class Game extends JPanel {
+public class Game extends JPanel implements ActionListener {
 
-	public static final int WIDTH = 300;
-	public static final int HEIGHT = 600;
-	public static final int SCORE_HEIGHT = 120;
+	public static final int WIDTH = 600;
+	public static final int HEIGHT = 800;
+        public static final int RIVER_HEIGHT = 220; 
 	
+        // game font
+        Font gameFont = new Font("Press Start 2P", Font.BOLD, 26);
+        
 	// game objects
 	private Player player;
+        private River river;
+        private Timer timer;
 
 	
 	public Game() {
-		player = new Player(WIDTH / 2, (HEIGHT - (player.getSize())), 40, Color.BLUE);
-		
+		player = new Player( 280, 760, 40, Color.WHITE);
+		river = new River(0, 70 ,WIDTH,RIVER_HEIGHT, Color.decode("#008dc7"));
+                timer = new Timer(30, this);
+                timer.start();
 	}
 	
 	@Override
@@ -27,16 +34,31 @@ public class Game extends JPanel {
 		super.paintComponent(g);
 
 		// ----------game design---------------------
-		setBackground(Color.WHITE);
+		setBackground(Color.decode("#cda360"));
 		player.draw(g);
-		
+                river.draw(g);
+               
+                /*draw game river*/
+                
 		
 		//collision detection
 		
 		
 		// ----------score design----------------------
-		
+		g.setColor(new Color(0, 0, 0, 30));
+                g.fillRect(0, 0, WIDTH, 70);
+                
+                g.setColor(Color.BLACK);
+                g.setFont(gameFont);
+                String score = Integer.toString(0);
+                g.drawString("Score: "+ score, 10, 48);
 		
 	}
 
+    @Override
+    public void actionPerformed(ActionEvent ae) {
+        river.Move();
+        repaint();
+    }
+     
 }
--- src/crazyrun/River.java
+++ src/crazyrun/River.java
@@ -1,5 +1,131 @@
 package crazyrun;
 
+import java.awt.Color;
+import java.awt.Graphics;
+import java.util.ArrayList;
+import java.util.Random;
+
 public class River {
+    private int x;
+
+   
+    private int y;
+    private int width ;
+    private int height;
+    private Color riverColor;
+    private  ArrayList<Corcodile> corcodiles = new ArrayList<Corcodile>();
+    private  ArrayList<Corcodile> corcodiles2 = new ArrayList<Corcodile>();
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public Color getRiverColor() {
+        return riverColor;
+    }
+
+    public void setRiverColor(Color riverColor) {
+        this.riverColor = riverColor;
+    }
+
+    public ArrayList<Corcodile> getCorcodiles() {
+        return corcodiles;
+    }
+
+    // getting random number for corcodile place 
+    Random rand = new Random();
+    
+    public River(int x , int y , int width , int height , Color color){
+        setX(x);
+        setY(y);
+        setWidth(width);
+        setHeight(height);
+        setRiverColor(color);
+        
+        for (int i = 0; i < 3; i++) {
+            int randX = rand.nextInt(900);
+            int randY = rand.nextInt(170)+70;
+            Corcodile corcodile = new Corcodile(100 , 20 , randX , randY , new Color(50, 80, 200, 255));
+            corcodiles.add(corcodile);
+        }
+        
+        for (int i = 0; i < 3; i++) {
+            int randX = rand.nextInt(900);
+            int randY = rand.nextInt(170)+70;
+            Corcodile corcodile = new Corcodile(100 , 20 , randX , randY , new Color(50, 80, 200, 255));
+            corcodiles2.add(corcodile);
+        }
+        
+    }
+    
+    public void draw(Graphics g){
+        g.setColor(getRiverColor());
+        g.fillRect(getX(), getY(), getWidth(), getHeight());
+        
+        for (int i = 0; i < corcodiles.size(); i++) {
+            corcodiles.get(i).draw(g);
+        }
+        
+        for (int j = 0; j < corcodiles2.size(); j++) {
+            corcodiles2.get(j).draw(g);
+        }
+    }
+    
+    public void Move(){
+        
+        // moving right
+        for (int i = 0; i < corcodiles.size(); i++) {
+            if(corcodiles.get(i).getX() + corcodiles.get(i).getWidth() > Game.WIDTH ) {
+                corcodiles.get(i).setX(rand.nextInt(1000)- (Game.WIDTH + 500));
+                corcodiles.get(i).setxSpeed(0);
+            }
+            else{
+                
+                corcodiles.get(i).moveRight(width, height);
+            }
+            
+        }
+        
+        // moving left
+        for (int j = 0; j < corcodiles2.size(); j++) {
+            if(corcodiles2.get(j).getX() + corcodiles2.get(j).getWidth() < Game.WIDTH - Game.WIDTH ) {
+                corcodiles2.get(j).setX(Game.WIDTH + corcodiles2.get(j).getWidth());
+                corcodiles2.get(j).setxSpeed(0);
+            }
+            else{
+                
+                corcodiles2.get(j).moveLeft(width, height);
+            }
+            
+        }
+    }
     
 }
